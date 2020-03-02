19ed2219208ea3a3893124a694e50b7c8f2440cc
kmrtll <kulmanmira@gmail.com>
2020-03-02 16:01:52 +0100

    Two new line

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -7,6 +7,8 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private int change = 0;
+	//Not conflict yet.
 
 	@Override
 	public void followSpeed() {
